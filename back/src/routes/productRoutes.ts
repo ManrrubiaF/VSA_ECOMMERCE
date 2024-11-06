e746f2068e6b106069e2ba031a00b41baca1f1da
ManrrubiaF <manrrubiaf@gmail.com>
2024-11-06 16:53:02 -0300

    add redirect

--- back/src/routes/productRoutes.ts
+++ back/src/routes/productRoutes.ts
@@ -13,36 +13,39 @@ productRouter.get('/:id', productHandler.getProductById);
 productRouter.get('/', accessMiddleware.accessValidation, productHandler.getAllProducts)
 productRouter.get('/preview/:id', async (req, res) => {
     const productId = req.params.id;
-    
+
     // Obtén los datos del producto desde tu base de datos o API
     const product = await productHandler.fetchProductById(productId);
 
     const defaultImage = "https://estudiosvsa.com.ar/assets/icons/vsaLOGO.jpg";
-const imageUrl = product?.details[0]?.image[0] || defaultImage;
+    const imageUrl = product?.details[0]?.image[0] || defaultImage;
+    const frontendUrl = `https://estudiosvsa.com.ar/products/${product?.id}` || "https://estudiosvsa.com.ar/"
 
-    if(product !== null){
+    if (product !== null) {
         res.send(`
             <!DOCTYPE html>
             <html lang="en">
               <head>
                 <meta charset="utf-8" />
+                <meta property="og:url" content="https://estudiosvsa.com.ar/products/${productId}" />
                 <meta property="og:title" content="${product.name}" />
                 <meta property="og:description" content="${product.description}" />
-                <meta property="og:image" content="${imageUrl}" />
-                <meta property="og:url" content="https://estudiosvsa.com.ar/products/${productId}" />
+                <meta property="og:image" content="${imageUrl}" />                
+                <meta property="og:type" content="website" />
                 <title>${product.name}</title>
               </head>
               <body>
-                <div id="root"></div>
-                <script src="/path/to/react-app.js"></script>
-              </body>
+        <script>
+          window.location.href = "${frontendUrl}";
+        </script>
+      </body>
             </html>
           `);
-    }else{
+    } else {
         return res.status(404).send("not found")
     }
-    
-    
-  });
-  
+
+
+});
+
 export default productRouter;
